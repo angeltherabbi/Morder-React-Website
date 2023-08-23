14c40b7ae2e234479a43d627169c5124e173e5d3
Angel <kharusoee@gmail.com>
2023-08-23 10:09:55 +0100

    first commit

---
+++ src/pages/gallery/Gallery.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './gallery.css'
+
+const Gallery = () => {
+  return (
+    <h1>gallary</h1>
+  )
+}
+
+export default Gallery
---
+++ src/pages/not_found/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './notfound.css'
+
+const NotFound = () => {
+  return (
+    <div>
+      Not found
+    </div>
+  )
+}
+
+export default NotFound
